1056a8b57900c7f13d770aa2d76470382f7a3ae5
PsychoFarid <faridarab4218@gmail.com>
2021-10-08 13:39:13 +0330

    Complete all CRUD methods.

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
-from todo.models import Todo, Todo_Pydantic, TodoIn_Pydantic
-from pydantic import BaseModel
 from tortoise.contrib.fastapi import register_tortoise
+from todo.views import todos_router
 
 app = FastAPI()
+
+app.include_router(todos_router,)
 
 
 @app.get('/')
 async def get_root():
     return {'Result': 'Value'}
-
-
-@app.post('/todos', response_model=Todo_Pydantic)
-async def create_todo(todo: TodoIn_Pydantic):
-    todo_obj = await Todo.create(**todo.dict(exclude_unset=True))
-    return await Todo_Pydantic.from_tortoise_orm(todo_obj)
-
-
-class Status(BaseModel):
-    message: str
 
 
 register_tortoise(
---
+++ todo/views.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, HTTPException
+from typing import List, Optional
+from pydantic import BaseModel
+from tortoise.contrib.fastapi import HTTPNotFoundError
+from todo.models import Todo, Todo_Pydantic, TodoIn_Pydantic
+
+todos_router = APIRouter(
+    prefix="/todos",
+    tags=["todos"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+class Status(BaseModel):
+    message: str
+
+
+@todos_router.post('/', response_model=Todo_Pydantic)
+async def create_todo(todo: TodoIn_Pydantic):
+    todo_obj = await Todo.create(**todo.dict(exclude_unset=True))
+    return await Todo_Pydantic.from_tortoise_orm(todo_obj)
+
+
+@todos_router.put('/{todo_id}', response_model=Todo_Pydantic, responses={404:{'model': HTTPNotFoundError}})
+async def update_todo(todo_id: int, todo: TodoIn_Pydantic):
+    await Todo.filter(id=todo_id).update(**todo.dict(exclude={"id"}, exclude_unset=True))
+    return await Todo_Pydantic.from_queryset_single(Todo.get(id=todo_id))
+
+
+@todos_router.delete('/{todo_id}', response_model=Status, responses={404:{'model': HTTPNotFoundError}})
+async def delete_todo(todo_id: int):
+    delete_count = await Todo.filter(id=todo_id).delete()
+    if not delete_count:
+        raise HTTPException(status_code=404, detail=f"Todo {todo_id} not found")
+    return Status(message=f"Deleted todo {todo_id}")
+
+
+@todos_router.get('/', response_model=List[Todo_Pydantic])
+async def get_todos():
+    return await Todo_Pydantic.from_queryset(Todo.all())
+
+
+@todos_router.get('/{todo_id}', response_model=Todo_Pydantic, responses={404:{'model': HTTPNotFoundError}})
+async def get_todo(todo_id:int):
+    return await Todo_Pydantic.from_queryset_single(Todo.get(id=todo_id))
